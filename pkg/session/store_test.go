855f1cbf027628034bd6e4ec92eed56baba4f2bf
Nikhil Soni <nikhilsoni@Nikhils-MacBook-Pro-2.local>
2020-12-27 16:46:06 +0530

    uncomment store unit test

--- pkg/session/store_test.go
+++ pkg/session/store_test.go
@@ -1,243 +1,271 @@
 package session_test
 
-//import (
-//	"context"
-//	"database/sql"
-//	"errors"
-//	"github.com/DATA-DOG/go-sqlmock"
-//	"github.com/stretchr/testify/require"
-//	"github.com/stretchr/testify/suite"
-//	"identification-service/pkg/database"
-//	"identification-service/pkg/session"
-//	"identification-service/pkg/test"
-//	"regexp"
-//	"strings"
-//	"testing"
-//	"time"
-//)
-//
-//type sessionStoreSuite struct {
-//	suite.Suite
-//	mock  sqlmock.Sqlmock
-//	db    database.SQLDatabase
-//	store session.Store
-//}
-//
-//func (st *sessionStoreSuite) SetupSuite() {
-//	sqlDB, mock := getMockDB(st.T())
-//
-//	st.db = database.NewSQLDatabase(sqlDB, test.QueryTTL)
-//	st.mock = mock
-//	st.store = session.NewStore(st.db)
-//}
-//
-//func (st *sessionStoreSuite) TestCreateSessionSuccess() {
-//	query := `insert into sessions (user_id, refresh_token) values ($1, $2) returning id`
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserID, test.SessionRefreshToken).
-//		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(test.SessionID))
-//
-//	s, err := session.NewSessionBuilder().UserID(test.UserID).RefreshToken(test.SessionRefreshToken).Build()
-//	require.NoError(st.T(), err)
-//
-//	_, err = st.store.CreateSession(context.Background(), s)
-//	require.NoError(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestCreateSessionFailure() {
-//	query := `insert into sessions (user_id, refresh_token) values ($1, $2) returning id`
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserID, test.SessionRefreshToken).
-//		WillReturnError(errors.New("failed to create session"))
-//
-//	s, err := session.NewSessionBuilder().UserID(test.UserID).RefreshToken(test.SessionRefreshToken).Build()
-//	require.NoError(st.T(), err)
-//
-//	_, err = st.store.CreateSession(context.Background(), s)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestGetSessionSuccess() {
-//	query := `select id, user_id, revoked, created_at, updated_at from sessions where refresh_token=$1`
-//
-//	rows := sqlmock.NewRows([]string{"id", "user_id", "revoked", "created_at", "updated_at"}).
-//		AddRow(test.SessionID, test.UserID, false, time.Time{}, time.Time{})
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.SessionRefreshToken).
-//		WillReturnRows(rows)
-//
-//	_, err := st.store.GetSession(context.Background(), test.SessionRefreshToken)
-//	require.NoError(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestGetSessionFailure() {
-//	query := `select id, user_id, revoked, created_at, updated_at from sessions where refresh_token=$1`
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.SessionRefreshToken).
-//		WillReturnError(errors.New("failed to get session"))
-//
-//	_, err := st.store.GetSession(context.Background(), test.SessionRefreshToken)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestGetActiveSessionsCountSuccess() {
-//	query := `select count(*) from sessions where user_id=$1 and revoked=false`
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserID).
-//		WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-//
-//	_, err := st.store.GetActiveSessionsCount(context.Background(), test.UserID)
-//	require.NoError(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestGetActiveSessionsCountFailure() {
-//	query := `select count(*) from sessions where user_id=$1 and revoked=false`
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserID).
-//		WillReturnError(errors.New("failed to get active sessions count"))
-//
-//	_, err := st.store.GetActiveSessionsCount(context.Background(), test.UserID)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeSessionsSuccess() {
-//	query := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
-//
-//	st.mock.ExpectExec(regexp.QuoteMeta(query)).
-//		WithArgs(toArgs([]string{test.SessionRefreshToken})).
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//
-//	_, err := st.store.RevokeSessions(context.Background(), test.SessionRefreshToken)
-//	require.NoError(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeSessionsFailure() {
-//	query := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
-//
-//	st.mock.ExpectExec(regexp.QuoteMeta(query)).
-//		WithArgs(toArgs([]string{test.SessionRefreshToken})).
-//		WillReturnError(errors.New("failed to revoke session"))
-//
-//	_, err := st.store.RevokeSessions(context.Background(), test.SessionRefreshToken)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeLastNSessionsSuccess() {
-//	fetchQuery := `select refresh_token from sessions where user_id=$1 and revoked=false order by created_at asc limit $2`
-//
-//	rows := sqlmock.NewRows([]string{"refresh_token"}).
-//		AddRow(test.SessionRefreshToken)
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(fetchQuery)).
-//		WithArgs(test.UserID, 1).
-//		WillReturnRows(rows)
-//
-//	execQuery := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
-//
-//	st.mock.ExpectExec(regexp.QuoteMeta(execQuery)).
-//		WithArgs(toArgs([]string{test.SessionRefreshToken})).
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//
-//	_, err := st.store.RevokeLastNSessions(context.Background(), test.UserID, 1)
-//	require.NoError(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeLastNSessionsFailureWhenFetchFails() {
-//	fetchQuery := `select refresh_token from sessions where user_id=$1 and revoked=false order by created_at asc limit $2`
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(fetchQuery)).
-//		WithArgs(test.UserID, 1).
-//		WillReturnError(errors.New("failed to fetch refresh tokens"))
-//
-//	_, err := st.store.RevokeLastNSessions(context.Background(), test.UserID, 1)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeLastNSessionsFailureWhenUpdateFails() {
-//	fetchQuery := `select refresh_token from sessions where user_id=$1 and revoked=false order by created_at asc limit $2`
-//
-//	rows := sqlmock.NewRows([]string{"refresh_token"}).
-//		AddRow(test.SessionRefreshToken)
-//
-//	st.mock.ExpectQuery(regexp.QuoteMeta(fetchQuery)).
-//		WithArgs(test.UserID, 1).
-//		WillReturnRows(rows)
-//
-//	execQuery := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
-//
-//	st.mock.ExpectExec(regexp.QuoteMeta(execQuery)).
-//		WithArgs(toArgs([]string{test.SessionRefreshToken})).
-//		WillReturnError(errors.New("failed to revoke sessions"))
-//
-//	_, err := st.store.RevokeLastNSessions(context.Background(), test.UserID, 1)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeAllSessionsSuccess() {
-//	query := `update sessions set revoked=true where user_id=$1`
-//
-//	st.mock.ExpectExec(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserID).
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//
-//	_, err := st.store.RevokeAllSessions(context.Background(), test.UserID)
-//	require.NoError(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func (st *sessionStoreSuite) TestRevokeAllSessionsFailure() {
-//	query := `update sessions set revoked=true where user_id=$1`
-//
-//	st.mock.ExpectExec(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserID).
-//		WillReturnError(errors.New("failed to revoke all sessions"))
-//
-//	_, err := st.store.RevokeAllSessions(context.Background(), test.UserID)
-//	require.Error(st.T(), err)
-//
-//	require.NoError(st.T(), st.mock.ExpectationsWereMet())
-//}
-//
-//func toArgs(values []string) string {
-//	return "{" + strings.Join(values, ",") + "}"
-//}
-//
-//func TestSessionStore(t *testing.T) {
-//	suite.Run(t, new(sessionStoreSuite))
-//}
-//
-//func getMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
-//	db, mock, err := sqlmock.New()
-//	require.NoError(t, err)
-//
-//	return db, mock
-//}
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"identification-service/pkg/database"
+	"identification-service/pkg/session"
+	"identification-service/pkg/test"
+	"regexp"
+	"strings"
+	"testing"
+	"time"
+)
+
+type sessionStoreSuite struct {
+	suite.Suite
+	mock  sqlmock.Sqlmock
+	db    database.SQLDatabase
+	store session.Store
+}
+
+func (st *sessionStoreSuite) SetupSuite() {
+	sqlDB, mock := getMockDB(st.T())
+
+	st.db = database.NewSQLDatabase(sqlDB, test.QueryTTL)
+	st.mock = mock
+	st.store = session.NewStore(st.db)
+}
+
+func (st *sessionStoreSuite) TestCreateSessionSuccess() {
+	userID, refreshToken := test.UserID(), test.SessionRefreshToken()
+
+	query := `insert into sessions (user_id, refresh_token) values ($1, $2) returning id`
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(userID, refreshToken).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(test.SessionID()))
+
+	s, err := session.NewSessionBuilder().UserID(userID).RefreshToken(refreshToken).Build()
+	require.NoError(st.T(), err)
+
+	_, err = st.store.CreateSession(context.Background(), s)
+	require.NoError(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestCreateSessionFailure() {
+	userID, refreshToken := test.UserID(), test.SessionRefreshToken()
+
+	query := `insert into sessions (user_id, refresh_token) values ($1, $2) returning id`
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(userID, refreshToken).
+		WillReturnError(errors.New("failed to create session"))
+
+	s, err := session.NewSessionBuilder().UserID(userID).RefreshToken(refreshToken).Build()
+	require.NoError(st.T(), err)
+
+	_, err = st.store.CreateSession(context.Background(), s)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestGetSessionSuccess() {
+	refreshToken := test.SessionRefreshToken()
+
+	query := `select id, user_id, revoked, created_at, updated_at from sessions where refresh_token=$1`
+
+	rows := sqlmock.NewRows([]string{"id", "user_id", "revoked", "created_at", "updated_at"}).
+		AddRow(test.SessionID(), test.UserID(), false, time.Time{}, time.Time{})
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(refreshToken).
+		WillReturnRows(rows)
+
+	_, err := st.store.GetSession(context.Background(), refreshToken)
+	require.NoError(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestGetSessionFailure() {
+	refreshToken := test.SessionRefreshToken()
+
+	query := `select id, user_id, revoked, created_at, updated_at from sessions where refresh_token=$1`
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(refreshToken).
+		WillReturnError(errors.New("failed to get session"))
+
+	_, err := st.store.GetSession(context.Background(), refreshToken)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestGetActiveSessionsCountSuccess() {
+	userID := test.UserID()
+
+	query := `select count(*) from sessions where user_id=$1 and revoked=false`
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(userID).
+		WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+
+	_, err := st.store.GetActiveSessionsCount(context.Background(), userID)
+	require.NoError(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestGetActiveSessionsCountFailure() {
+	userID := test.UserID()
+
+	query := `select count(*) from sessions where user_id=$1 and revoked=false`
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(userID).
+		WillReturnError(errors.New("failed to get active sessions count"))
+
+	_, err := st.store.GetActiveSessionsCount(context.Background(), userID)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeSessionsSuccess() {
+	refreshToken := test.SessionRefreshToken()
+
+	query := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
+
+	st.mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(toArgs([]string{refreshToken})).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err := st.store.RevokeSessions(context.Background(), refreshToken)
+	require.NoError(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeSessionsFailure() {
+	refreshToken := test.SessionRefreshToken()
+
+	query := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
+
+	st.mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(toArgs([]string{refreshToken})).
+		WillReturnError(errors.New("failed to revoke session"))
+
+	_, err := st.store.RevokeSessions(context.Background(), refreshToken)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeLastNSessionsSuccess() {
+	userID := test.UserID()
+	refreshToken := test.SessionRefreshToken()
+
+	fetchQuery := `select refresh_token from sessions where user_id=$1 and revoked=false order by created_at asc limit $2`
+
+	rows := sqlmock.NewRows([]string{"refresh_token"}).
+		AddRow(refreshToken)
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(fetchQuery)).
+		WithArgs(userID, 1).
+		WillReturnRows(rows)
+
+	execQuery := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
+
+	st.mock.ExpectExec(regexp.QuoteMeta(execQuery)).
+		WithArgs(toArgs([]string{refreshToken})).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err := st.store.RevokeLastNSessions(context.Background(), userID, 1)
+	require.NoError(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeLastNSessionsFailureWhenFetchFails() {
+	userID := test.UserID()
+
+	fetchQuery := `select refresh_token from sessions where user_id=$1 and revoked=false order by created_at asc limit $2`
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(fetchQuery)).
+		WithArgs(userID, 1).
+		WillReturnError(errors.New("failed to fetch refresh tokens"))
+
+	_, err := st.store.RevokeLastNSessions(context.Background(), userID, 1)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeLastNSessionsFailureWhenUpdateFails() {
+	userID := test.UserID()
+	refreshToken := test.SessionRefreshToken()
+
+	fetchQuery := `select refresh_token from sessions where user_id=$1 and revoked=false order by created_at asc limit $2`
+
+	rows := sqlmock.NewRows([]string{"refresh_token"}).
+		AddRow(refreshToken)
+
+	st.mock.ExpectQuery(regexp.QuoteMeta(fetchQuery)).
+		WithArgs(userID, 1).
+		WillReturnRows(rows)
+
+	execQuery := `update sessions set revoked=true where refresh_token = ANY($1::uuid[])`
+
+	st.mock.ExpectExec(regexp.QuoteMeta(execQuery)).
+		WithArgs(toArgs([]string{refreshToken})).
+		WillReturnError(errors.New("failed to revoke sessions"))
+
+	_, err := st.store.RevokeLastNSessions(context.Background(), userID, 1)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeAllSessionsSuccess() {
+	userID := test.UserID()
+
+	query := `update sessions set revoked=true where user_id=$1`
+
+	st.mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(userID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err := st.store.RevokeAllSessions(context.Background(), userID)
+	require.NoError(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func (st *sessionStoreSuite) TestRevokeAllSessionsFailure() {
+	userID := test.UserID()
+
+	query := `update sessions set revoked=true where user_id=$1`
+
+	st.mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(userID).
+		WillReturnError(errors.New("failed to revoke all sessions"))
+
+	_, err := st.store.RevokeAllSessions(context.Background(), userID)
+	require.Error(st.T(), err)
+
+	require.NoError(st.T(), st.mock.ExpectationsWereMet())
+}
+
+func toArgs(values []string) string {
+	return "{" + strings.Join(values, ",") + "}"
+}
+
+func TestSessionStore(t *testing.T) {
+	suite.Run(t, new(sessionStoreSuite))
+}
+
+func getMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	return db, mock
+}
--- pkg/user/store_test.go
+++ pkg/user/store_test.go
@@ -1,152 +1,177 @@
 package user_test
 
-//import (
-//	"context"
-//	"database/sql"
-//	"errors"
-//	"github.com/DATA-DOG/go-sqlmock"
-//	"github.com/stretchr/testify/require"
-//	"github.com/stretchr/testify/suite"
-//	"identification-service/pkg/database"
-//	"identification-service/pkg/password"
-//	"identification-service/pkg/test"
-//	"identification-service/pkg/user"
-//	"regexp"
-//	"testing"
-//)
-//
-//type userStoreSuite struct {
-//	suite.Suite
-//	db    database.SQLDatabase
-//	mock  sqlmock.Sqlmock
-//	store user.Store
-//}
-//
-//func (ust *userStoreSuite) SetupSuite() {
-//	sqlDB, mock := getMockDB(ust.T())
-//
-//	ust.db = database.NewSQLDatabase(sqlDB, test.QueryTTL)
-//	ust.mock = mock
-//
-//	ust.store = user.NewStore(ust.db)
-//}
-//
-//func (ust *userStoreSuite) TestCreateUserSuccess() {
-//	query := `insert into users (name, email, password_hash, password_salt) values ($1, $2, $3, $4) returning id`
-//
-//	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserName, test.UserEmail, test.UserPasswordHash, test.UserPasswordSalt).
-//		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(test.UserID))
-//
-//	currUser := getUser(ust.T(), test.UserName, test.UserEmail, test.UserPassword)
-//
-//	_, err := ust.store.CreateUser(context.Background(), currUser)
-//	require.NoError(ust.T(), err)
-//
-//	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
-//}
-//
-//func (ust *userStoreSuite) TestCreateUserFailure() {
-//	query := `insert into users (name, email, password_hash, password_salt) values ($1, $2, $3, $4) returning id`
-//
-//	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserName, test.UserEmail, test.UserPasswordHash, test.UserPasswordSalt).
-//		WillReturnError(errors.New("failed to create new User"))
-//
-//	currUser := getUser(ust.T(), test.UserName, test.UserEmail, test.UserPassword)
-//
-//	_, err := ust.store.CreateUser(context.Background(), currUser)
-//	require.Error(ust.T(), err)
-//
-//	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
-//}
-//
-//func (ust *userStoreSuite) TestGetUserSuccess() {
-//	query := `select id, name, email, password_hash, password_salt from users where email = $1`
-//
-//	rows := sqlmock.NewRows(
-//		[]string{
-//			"id", "name", "email", "passwordhash", "passwordsalt",
-//		},
-//	)
-//
-//	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserEmail).
-//		WillReturnRows(rows.AddRow("", "", "", "", ""))
-//
-//	us := user.NewStore(ust.db)
-//
-//	_, err := us.GetUser(context.Background(), test.UserEmail)
-//	require.NoError(ust.T(), err)
-//
-//	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
-//}
-//
-//func (ust *userStoreSuite) TestGetUserFailure() {
-//	query := `select id, name, email, password_hash, password_salt from users where email = $1`
-//
-//	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserEmail).
-//		WillReturnError(errors.New("failed to get data"))
-//
-//	us := user.NewStore(ust.db)
-//
-//	_, err := us.GetUser(context.Background(), test.UserEmail)
-//	require.Error(ust.T(), err)
-//
-//	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
-//}
-//
-//func (ust *userStoreSuite) TestUpdatePasswordSuccess() {
-//	query := `update users set password_hash=$1, password_salt=$2 where id=$3`
-//
-//	ust.mock.ExpectExec(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserPasswordHash, test.UserPasswordSalt, test.UserEmail).
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//
-//	us := user.NewStore(ust.db)
-//
-//	_, err := us.UpdatePassword(context.Background(), test.UserEmail, test.UserPasswordHash, test.UserPasswordSalt)
-//	require.NoError(ust.T(), err)
-//
-//	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
-//}
-//
-//func (ust *userStoreSuite) TestUpdatePasswordFailure() {
-//	query := `update users set password_hash=$1, password_salt=$2 where id=$3`
-//
-//	ust.mock.ExpectExec(regexp.QuoteMeta(query)).
-//		WithArgs(test.UserPasswordHash, test.UserPasswordSalt, test.UserEmail).
-//		WillReturnError(errors.New("failed to update password"))
-//
-//	us := user.NewStore(ust.db)
-//
-//	_, err := us.UpdatePassword(context.Background(), test.UserEmail, test.UserPasswordHash, test.UserPasswordSalt)
-//	require.Error(ust.T(), err)
-//
-//	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
-//}
-//
-//func TestStore(t *testing.T) {
-//	suite.Run(t, new(userStoreSuite))
-//}
-//
-//func getUser(t *testing.T, name, email, userPassword string) user.User {
-//	mockEncoder := &password.MockEncoder{}
-//	mockEncoder.On("GenerateSalt").Return(test.UserPasswordSalt, nil)
-//	mockEncoder.On("GenerateKey", userPassword, test.UserPasswordSalt).Return(test.UserPasswordKey)
-//	mockEncoder.On("EncodeKey", test.UserPasswordKey).Return(test.UserPasswordHash)
-//	mockEncoder.On("ValidatePassword", userPassword).Return(nil)
-//
-//	us, err := user.NewUserBuilder(mockEncoder).Name(name).Email(email).Password(userPassword).Build()
-//	require.NoError(t, err)
-//
-//	return us
-//}
-//
-//func getMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
-//	db, mock, err := sqlmock.New()
-//	require.NoError(t, err)
-//
-//	return db, mock
-//}
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"identification-service/pkg/database"
+	"identification-service/pkg/password"
+	"identification-service/pkg/test"
+	"identification-service/pkg/user"
+	"regexp"
+	"testing"
+)
+
+type userStoreSuite struct {
+	suite.Suite
+	db    database.SQLDatabase
+	mock  sqlmock.Sqlmock
+	store user.Store
+}
+
+func (ust *userStoreSuite) SetupSuite() {
+	sqlDB, mock := getMockDB(ust.T())
+
+	ust.db = database.NewSQLDatabase(sqlDB, test.QueryTTL)
+	ust.mock = mock
+
+	ust.store = user.NewStore(ust.db)
+}
+
+func (ust *userStoreSuite) TestCreateUserSuccess() {
+	name, email := test.UserName(), test.UserEmail()
+
+	passwordSalt := test.UserPasswordSalt()
+	passwordKey := test.UserPasswordKey()
+	passwordHash := test.UserPasswordHash()
+
+	query := `insert into users (name, email, password_hash, password_salt) values ($1, $2, $3, $4) returning id`
+
+	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(name, email, passwordHash, passwordSalt).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(test.UserID()))
+
+	mockEncoder := &password.MockEncoder{}
+	mockEncoder.On("GenerateSalt").Return(passwordSalt, nil)
+	mockEncoder.On("GenerateKey", test.UserPassword, passwordSalt).Return(passwordKey)
+	mockEncoder.On("EncodeKey", passwordKey).Return(passwordHash)
+	mockEncoder.On("ValidatePassword", test.UserPassword).Return(nil)
+
+	currUser, err := user.NewUserBuilder(mockEncoder).Name(name).Email(email).Password(test.UserPassword).Build()
+	require.NoError(ust.T(), err)
+
+	_, err = ust.store.CreateUser(context.Background(), currUser)
+	require.NoError(ust.T(), err)
+
+	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
+}
+
+func (ust *userStoreSuite) TestCreateUserFailure() {
+	name, email := test.UserName(), test.UserEmail()
+
+	passwordSalt := test.UserPasswordSalt()
+	passwordKey := test.UserPasswordKey()
+	passwordHash := test.UserPasswordHash()
+
+	query := `insert into users (name, email, password_hash, password_salt) values ($1, $2, $3, $4) returning id`
+
+	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(name, email, passwordHash, passwordSalt).
+		WillReturnError(errors.New("failed to create new User"))
+
+	mockEncoder := &password.MockEncoder{}
+	mockEncoder.On("GenerateSalt").Return(passwordSalt, nil)
+	mockEncoder.On("GenerateKey", test.UserPassword, passwordSalt).Return(passwordKey)
+	mockEncoder.On("EncodeKey", passwordKey).Return(passwordHash)
+	mockEncoder.On("ValidatePassword", test.UserPassword).Return(nil)
+
+	currUser, err := user.NewUserBuilder(mockEncoder).Name(name).Email(email).Password(test.UserPassword).Build()
+	require.NoError(ust.T(), err)
+
+	_, err = ust.store.CreateUser(context.Background(), currUser)
+	require.Error(ust.T(), err)
+
+	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
+}
+
+func (ust *userStoreSuite) TestGetUserSuccess() {
+	userEmail := test.UserEmail()
+
+	query := `select id, name, email, password_hash, password_salt from users where email = $1`
+
+	rows := sqlmock.NewRows(
+		[]string{
+			"id", "name", "email", "passwordhash", "passwordsalt",
+		},
+	)
+
+	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(userEmail).
+		WillReturnRows(rows.AddRow("", "", "", "", ""))
+
+	us := user.NewStore(ust.db)
+
+	_, err := us.GetUser(context.Background(), userEmail)
+	require.NoError(ust.T(), err)
+
+	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
+}
+
+func (ust *userStoreSuite) TestGetUserFailure() {
+	userEmail := test.UserEmail()
+
+	query := `select id, name, email, password_hash, password_salt from users where email = $1`
+
+	ust.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(userEmail).
+		WillReturnError(errors.New("failed to get data"))
+
+	us := user.NewStore(ust.db)
+
+	_, err := us.GetUser(context.Background(), userEmail)
+	require.Error(ust.T(), err)
+
+	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
+}
+
+func (ust *userStoreSuite) TestUpdatePasswordSuccess() {
+	email := test.UserEmail()
+	passwordSalt := test.UserPasswordSalt()
+	passwordHash := test.UserPasswordHash()
+
+	query := `update users set password_hash=$1, password_salt=$2 where id=$3`
+
+	ust.mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(passwordHash, passwordSalt, email).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	us := user.NewStore(ust.db)
+
+	_, err := us.UpdatePassword(context.Background(), email, passwordHash, passwordSalt)
+	require.NoError(ust.T(), err)
+
+	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
+}
+
+func (ust *userStoreSuite) TestUpdatePasswordFailure() {
+	email := test.UserEmail()
+	passwordSalt := test.UserPasswordSalt()
+	passwordHash := test.UserPasswordHash()
+
+	query := `update users set password_hash=$1, password_salt=$2 where id=$3`
+
+	ust.mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(passwordHash, passwordSalt, email).
+		WillReturnError(errors.New("failed to update password"))
+
+	us := user.NewStore(ust.db)
+
+	_, err := us.UpdatePassword(context.Background(), email, passwordHash, passwordSalt)
+	require.Error(ust.T(), err)
+
+	require.NoError(ust.T(), ust.mock.ExpectationsWereMet())
+}
+
+func TestStore(t *testing.T) {
+	suite.Run(t, new(userStoreSuite))
+}
+
+func getMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	return db, mock
+}
